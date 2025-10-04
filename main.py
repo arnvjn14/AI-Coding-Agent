5d7a43ece308d105fbd18c9069ba4c092c874bf6
arnv <arnavj343@gmail.com>
2025-10-04 12:29:34 +0530

    initial gemini api setup

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+import os 
+import sys
+from google import genai
+from dotenv import load_dotenv
+from google.genai import types
+
+
 def main():
-    print("Hello from coding-agent!")
+
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    print("Arguments:", sys.argv)
+
+    if len(sys.argv) < 2:
+        print("Please provide a prompt as a command-line argument.")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+
+    messages= [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
 
-if __name__ == "__main__":
-    main()
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("No response or usage metadata found.")
+        return
+    
+    print("Prompt Tokens:", response.usage_metadata.prompt_token_count)
+    print("Completion Tokens:", response.usage_metadata.candidates_token_count)
+
+main()
+    
+
